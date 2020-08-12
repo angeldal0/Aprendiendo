61529144f32a2c503c4cb165e19093a444d719eb
angeldal0 <65708300+angeldal0@users.noreply.github.com>
2020-08-12 03:53:30 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,10 +2,28 @@
 /*                     script de las listas                        */
 /*******************************************************************/
 
+var ultimaLista = "listaBebidas";
+
 function mostrarLista(lista){
     switch (lista.innerHTML){
         case "Bebidas":
+            ocultarAnterior(ultimaLista);
             var listaBebidas = document.getElementById("listaBebidas").style.display = "block";
+            ultimaLista = "listaBebidas";
             break;
+        case "Entrantes":
+            ocultarAnterior(ultimaLista);
+            var listaEntrantes = document.getElementById("listaEntrantes").style.display = "block";
+            ultimaLista = "listaEntrantes";
+            break;
+        case "Hamburguesas":
+            ocultarAnterior(ultimaLista);
+            var listaHamburguesas = document.getElementById("listaHamburguesas").style.display = "block";
+            ultimaLista = "listaHamburguesas";
     }
+}
+
+function ocultarAnterior(){
+    var x = document.getElementById(ultimaLista);
+    x.style.display = "none";
 }
