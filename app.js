a1616cb0078453aa13cac835e674cb9075c9bab7
angeldal0 <65708300+angeldal0@users.noreply.github.com>
2020-08-12 03:30:15 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+/*******************************************************************/
+/*                     script de las listas                        */
+/*******************************************************************/
+
+function mostrarLista(lista){
+    switch (lista.innerHTML){
+        case "Bebidas":
+            var listaBebidas = document.getElementById("listaBebidas").style.display = "block";
+            break;
+    }
+}
