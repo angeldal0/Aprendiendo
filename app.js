e5886ca91a2aa45dd68f2781cdebfe58864444bc
angeldal0 <65708300+angeldal0@users.noreply.github.com>
2020-08-16 03:34:45 +0200

    Update app.js

--- app.js
+++ app.js
@@ -5,6 +5,7 @@
 var ultimaLista = "listaEntrantes";
 
 function mostrarLista(lista){
+    ocultarComanda();
     switch (lista.innerHTML){
         case "Bebidas":
             ocultarAnterior(ultimaLista);
@@ -26,4 +27,36 @@ function mostrarLista(lista){
 function ocultarAnterior(){
     var x = document.getElementById(ultimaLista);
     x.style.display = "none";
+}
+
+var ultimaComanda = document.getElementById("ultimaComanda");
+var comandas = [];
+var lista = document.getElementById("comandas");
+var listaComandas = document.getElementById("listaComandas");
+
+function mostrarComanda(){
+    ocultarAnterior(ultimaLista);
+    lista.style.display = "block";
+}
+
+function ocultarComanda(){
+    lista.style.display = "none";
+}
+
+function añadirComanda(cosa){
+    comandas.push(cosa.innerHTML);
+    var nuevaComanda = document.createElement("li");
+    nuevaComanda.id = 'c' + comandas.length;
+    ultimaComanda.innerHTML = nuevaComanda.innerHTML = cosa.innerHTML;
+    listaComandas.appendChild(nuevaComanda);
+}
+
+function eliminarUltimaComanda(){
+    listaComandas.removeChild(document.getElementById('c' + comandas.length));
+    comandas.pop();
+    if (comandas.length == 0){
+        ultimaComanda.innerHTML = "No hay comanda";
+    } else {
+        ultimaComanda.innerHTML = comandas[comandas.length - 1];
+    }
 }
